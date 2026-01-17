46fa91952a90c155905d4e5e3ce5914a21148658
fatihaosman <fatihaosman17@gmail.com>
2026-01-17 07:03:55 +0300

    fixing layour errors

--- prodev-mobile-app-0x06/app/(home)/layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/layout.tsx
@@ -5,7 +5,7 @@ const HomeRootLayout = () => {
   return (
     <Tabs screenOptions={{
         tabBarActiveTintColor: '#34967C',
-        headerShown: false
+        headerShown: false // default for all tabs
     }} >
       <Tabs.Screen name="index" options={{
         title: 'Home',
@@ -21,24 +21,18 @@ const HomeRootLayout = () => {
         headerShown: true,
         tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />
       }} />
-        <Tabs.Screen name="inbox" options={{
+      <Tabs.Screen name="inbox" options={{
         title: 'Inbox',
         headerShown: true,
         tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
       }} />
       <Tabs.Screen name="profile" options={{
-    title: 'Profile',
-    tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
-}} />
-
-        {/* <Tabs.Screen name="profile" options={{
         title: 'Profile',
-        headerShown: true,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
-      }} /> */}
+      }} />
     </Tabs>
-    
   )
 }
 
 export default HomeRootLayout;
+
