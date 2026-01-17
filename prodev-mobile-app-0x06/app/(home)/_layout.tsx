80b4bdf3faaa08ce30b4e09ce01b3280261e7d6e
fatihaosman <fatihaosman17@gmail.com>
2026-01-17 07:32:10 +0300

    fixing layout

--- prodev-mobile-app-0x06/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/_layout.tsx
@@ -26,9 +26,12 @@ const HomeRootLayout = () => {
         headerShown: true,
         tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
       }} />
-        <Tabs.Screen name="profile" options={{
+      <Tabs.Screen
+      name="profile"
+      options={{
         title: 'Profile',
-        tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
+        // Notice: NO headerShown here
+        tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color={color} />,
       }} />
     </Tabs>
   )
